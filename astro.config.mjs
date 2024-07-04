cc61c85b0c278f825c3f1d9a863a2f2f3a38d02c
Emil Gulamov <125820963+mearashadowfax@users.noreply.github.com>
2024-07-05 02:42:41 +0400

    Update astro configuration, add new icon, and remove vercel.json
    This commit introduces several changes to the astro configuration, including importing the netlify plugin and updating various dependencies. Additionally, it adds a new icon for the application. Lastly, it removes the vercel.json file.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,104 +1,120 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercelStatic from "@astrojs/vercel/static";
 import sitemap from "@astrojs/sitemap";
 import compressor from "astro-compressor";
 import starlight from "@astrojs/starlight";
+
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   // https://docs.astro.build/en/guides/images/#authorizing-remote-images
   site: "https://screwfast.uk",
   image: {
-    domains: ["images.unsplash.com"],
+    domains: ["images.unsplash.com"]
   },
   prefetch: true,
-  integrations: [
-    tailwind(),
-    sitemap(),
-    starlight({
-      title: "ScrewFast Docs",
-      defaultLocale: "root",
-      locales: {
-        root: {
-          label: "English",
-          lang: "en",
-        },
-        de: { label: "Deutsch", lang: "de" },
-        es: { label: "Español", lang: "es" },
-        fa: { label: "Persian", lang: "fa", dir: "rtl" },
-        fr: { label: "Français", lang: "fr" },
-        ja: { label: "日本語", lang: "ja" },
-        "zh-cn": { label: "简体中文", lang: "zh-CN" },
+  integrations: [tailwind(), sitemap(), starlight({
+    title: "ScrewFast Docs",
+    defaultLocale: "root",
+    locales: {
+      root: {
+        label: "English",
+        lang: "en"
       },
-      // https://starlight.astro.build/guides/sidebar/
-      sidebar: [
-        {
-          label: "Quick Start Guides",
-          translations: {
-            de: "Schnellstartanleitungen",
-            es: "Guías de Inicio Rápido",
-            fa: "راهنمای شروع سریع",
-            fr: "Guides de Démarrage Rapide",
-            ja: "クイックスタートガイド",
-            "zh-cn": "快速入门指南",
-          },
-          autogenerate: { directory: "guides" },
-        },
-        {
-          label: "Tools & Equipment",
-          items: [
-            { label: "Tool Guides", link: "tools/tool-guides/" },
-            { label: "Equipment Care", link: "tools/equipment-care/" },
-          ],
-        },
-        {
-          label: "Construction Services",
-          autogenerate: { directory: "construction" },
-        },
-        {
-          label: "Advanced Topics",
-          autogenerate: { directory: "advanced" },
-        },
-      ],
-      social: {
-        github: "https://github.com/mearashadowfax/ScrewFast",
+      de: {
+        label: "Deutsch",
+        lang: "de"
       },
-      disable404Route: true,
-      customCss: ["./src/assets/styles/starlight.css"],
-      favicon: "/favicon.ico",
-      components: {
-        SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
-        Head: "./src/components/ui/starlight/Head.astro",
-        MobileMenuFooter: "./src/components/ui/starlight/MobileMenuFooter.astro",
-        ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro",
+      es: {
+        label: "Español",
+        lang: "es"
       },
-      head: [
-        {
-          tag: "meta",
-          attrs: {
-            property: "og:image",
-            content: "https://screwfast.uk" + "/social.webp",
-          },
-        },
-        {
-          tag: "meta",
-          attrs: {
-            property: "twitter:image",
-            content: "https://screwfast.uk" + "/social.webp",
-          },
-        },
-      ],
-    }),
-    compressor({
-      gzip: false,
-      brotli: true,
-    }),
-  ],
+      fa: {
+        label: "Persian",
+        lang: "fa",
+        dir: "rtl"
+      },
+      fr: {
+        label: "Français",
+        lang: "fr"
+      },
+      ja: {
+        label: "日本語",
+        lang: "ja"
+      },
+      "zh-cn": {
+        label: "简体中文",
+        lang: "zh-CN"
+      }
+    },
+    // https://starlight.astro.build/guides/sidebar/
+    sidebar: [{
+      label: "Quick Start Guides",
+      translations: {
+        de: "Schnellstartanleitungen",
+        es: "Guías de Inicio Rápido",
+        fa: "راهنمای شروع سریع",
+        fr: "Guides de Démarrage Rapide",
+        ja: "クイックスタートガイド",
+        "zh-cn": "快速入门指南"
+      },
+      autogenerate: {
+        directory: "guides"
+      }
+    }, {
+      label: "Tools & Equipment",
+      items: [{
+        label: "Tool Guides",
+        link: "tools/tool-guides/"
+      }, {
+        label: "Equipment Care",
+        link: "tools/equipment-care/"
+      }]
+    }, {
+      label: "Construction Services",
+      autogenerate: {
+        directory: "construction"
+      }
+    }, {
+      label: "Advanced Topics",
+      autogenerate: {
+        directory: "advanced"
+      }
+    }],
+    social: {
+      github: "https://github.com/mearashadowfax/ScrewFast"
+    },
+    disable404Route: true,
+    customCss: ["./src/assets/styles/starlight.css"],
+    favicon: "/favicon.ico",
+    components: {
+      SiteTitle: "./src/components/ui/starlight/SiteTitle.astro",
+      Head: "./src/components/ui/starlight/Head.astro",
+      MobileMenuFooter: "./src/components/ui/starlight/MobileMenuFooter.astro",
+      ThemeSelect: "./src/components/ui/starlight/ThemeSelect.astro"
+    },
+    head: [{
+      tag: "meta",
+      attrs: {
+        property: "og:image",
+        content: "https://screwfast.uk" + "/social.webp"
+      }
+    }, {
+      tag: "meta",
+      attrs: {
+        property: "twitter:image",
+        content: "https://screwfast.uk" + "/social.webp"
+      }
+    }]
+  }), compressor({
+    gzip: false,
+    brotli: true
+  })],
   output: "static",
   experimental: {
     clientPrerender: true,
-    directRenderScript: true,
+    directRenderScript: true
   },
-  adapter: vercelStatic(),
+  adapter: netlify()
 });
